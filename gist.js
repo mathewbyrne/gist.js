9cc2b075722f03c888dc067416f487e2d8f9dc54
Mathew Byrne <mathewbyrne@gmail.com>
2010-07-30 13:39:09 +1000

    Refactored gist.js to allow a custom URL.
    Also added a test html page, should probably contain some better tests eventually.

--- gist.js
+++ gist.js
@@ -3,24 +3,27 @@
 
 var count = 1;
 
-$.gist = function (id, file) {
- 	var div = 'gist-' + id + '-' + count++
-	document.write('<div id="' + div + '"></div>');
-	$('#' + div).gist(id, file);
+$.gist = function (id, file, placeholder) {
+ 	placeholder = placeholder || ['gist', id, count++].join('-');
+	$('<div />', { 'id': placeholder }).appendTo('body').gist(id, file);
+};
+
+$.gist.url = function (id, file) {
+	var url = 'http://gist.github.com/' + id + '.json?callback=?';
+	if (file) {
+		url += '&file=' + file;
+	}
+	return url;
 };
 
 $.fn.gist = function (id, file) {
 	
-	var url   = 'http://gist.github.com/' + id + '.json',
-		$this = $(this);
-	
-	if (file) {
-		url += '?file=' + file;
-	}
+	var self = this,
+		url  = $.gist.url.apply(this, arguments);
 	
 	$.getJSON(url, function (json) {
 			$(json.div)
-				.replaceAll($this)
+				.replaceAll(self)
 				.trigger('gistloaded', json);
 		});
 	
